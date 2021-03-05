821acfc92e2e2f501dbedfd8c65f1bc24dc5f820
27akim <criminal.ok1@gmail.com>
2021-03-05 14:46:42 +0300

    feat: add team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeamName = '';
+  for(let i = 0; i < members.length; i++)
+  {
+    if(typeof members[i] != 'string')
+    {
+      continue;
+    }
+    dreamTeamName += members[i].trim()[0].toUpperCase();
+  }
+  if(dreamTeamName == '')
+  {
+    return false;
+  }
+  return dreamTeamName.split('').sort().join('');
 };
