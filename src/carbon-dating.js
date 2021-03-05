1c597d45e55dcfa2c9a4a497d157c153b131362a
27akim <criminal.ok1@gmail.com>
2021-03-05 14:27:54 +0300

    feat: add carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,12 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-/*module.exports =*/ function dateSample(sampleActivity) {
+module.exports = function dateSample(sampleActivity) {
   if(typeof sampleActivity != 'string' || isNaN(parseInt(sampleActivity)) || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY)
   {
     return false;
   }
   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.LN2 / HALF_LIFE_PERIOD))
 };
-
-console.log(dateSample('299'));
