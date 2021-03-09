7125dea8dbb96660a91809ee1096099fc7561e4e
27akim <criminal.ok1@gmail.com>
2021-03-09 10:52:16 +0300

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater( str, options) {
+/*module.exports =*/ function repeater( str, options) {
   let resultStr = '';
   if(options.separator == undefined)
   {
@@ -8,15 +8,30 @@ module.exports = function repeater( str, options) {
   }
   if(options.additionSeparator == undefined)
   {
-    options.additionSeparator = '|';
+    options.additionSeparator = '';
+  }
+  if(options.repeatTimes == undefined)
+  {
+    options.repeatTimes = 1;
+  }
+  if(options.additionRepeatTimes == undefined)
+  {
+    options.additionRepeatTimes = 1;
+  }
+  if(options.addition == undefined)
+  {
+    options.addition = '';
   }
   for(let i = 0; i < options.repeatTimes; i++)
   {
     resultStr += str;
     for(let j = 0; j < options.additionRepeatTimes; j++)
     {
-      resultStr = resultStr + options.addition + 
-      options.additionSeparator;
+      resultStr = resultStr + options.addition;
+      if(i != options.additionRepeatTimes - 1)
+      {
+        options.additionSeparator;
+      }
     }
     if(i != options.repeatTimes - 1)
     {
@@ -25,3 +40,6 @@ module.exports = function repeater( str, options) {
   }
   return resultStr;
 }
+
+console.log(repeater('la', { repeatTimes: 3, separator: 's', addition: '+', additionRepeatTimes: 1 }));
+  
