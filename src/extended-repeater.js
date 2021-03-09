08a7f6143c805aa0b7891295d32d21a141e9c1ee
27akim <criminal.ok1@gmail.com>
2021-03-09 10:22:18 +0300

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater( str, options) {
+  let resultStr = '';
+  if(options.separator == undefined)
+  {
+    options.separator = '+';
+  }
+  if(options.additionSeparator == undefined)
+  {
+    options.additionSeparator = '|';
+  }
+  for(let i = 0; i < options.repeatTimes; i++)
+  {
+    resultStr += str;
+    for(let j = 0; j < options.additionRepeatTimes; j++)
+    {
+      resultStr = resultStr + options.addition + 
+      options.additionSeparator;
+    }
+    resultStr += options.separator;
+  }
+  return resultStr;
+}
   
