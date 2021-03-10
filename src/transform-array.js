9859fc9d8cfaab02ef3aa1d336218a1319af75a2
27akim <criminal.ok1@gmail.com>
2021-03-10 13:38:12 +0300

    feat: add transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(typeof arr != Array)
+  {
+    throw new Error();
+  }
+  for(let i = 0; i < arr.length; i++)
+  {
+    if(arr[i] == '--discard-next')
+    {
+      try{
+        arr.splice(i+1,1);
+      }
+      catch(e)
+      {
+        continue;
+      }
+    }
+    if(arr[i] == '--discard-prev')
+    {
+      try{
+        arr.splice(i-1,1);
+      }
+      catch(e)
+      {
+        continue;
+      }
+    }
+    if(arr[i] == '--double-next')
+    {
+      try{
+        arr[i] = arr[i+1];
+      }
+      catch(e)
+      {
+        continue;
+      }
+    }
+    if(arr[i] == '--double-prev')
+    {
+      try{
+        arr[i] = arr[i-1];
+      }
+      catch(e)
+      {
+        continue;
+      }
+    }
+  }
+  return arr;
 };
