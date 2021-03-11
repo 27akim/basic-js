71ccc537057b7b3965c10b115bf578e29edcbfcd
27akim <criminal.ok1@gmail.com>
2021-03-11 15:15:37 +0300

    feat: add vagenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  N = this.alphabet.length;
+  machineType = 'direct';
+  flag = true;
+
+  constructor(flag){
+    if(flag !== undefined)
+    {
+      this.flag = flag;
+    }
+  }
+
+  encrypt(message, key) {
+    if(message === undefined || key === undefined)
+    {
+      throw new Error();
+    }
+    let result = '';
+    let keywordIndex = 0;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for(let c of message)
+    {
+      if(this.alphabet.indexOf(c) >= 0)
+      {
+        let n = (this.alphabet.indexOf(c) + this.alphabet.indexOf(key[keywordIndex])) % this.N;
+        result += this.alphabet[n]; 
+      }
+      else
+      {
+        result += c;
+        continue;
+      }
+      keywordIndex++;
+      if(keywordIndex == key.length)
+      {
+        keywordIndex = 0;
+      }
+    }
+    return this.flag ? result : result.split('').reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    if(encryptedMessage === undefined || key === undefined)
+    {
+      throw new Error();
+    }
+    let result = '';
+    let keywordIndex = 0;
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    for(let c of encryptedMessage)
+    {
+      if(this.alphabet.indexOf(c) >= 0)
+      {
+        let n = (this.alphabet.indexOf(c) + this.N - this.alphabet.indexOf(key[keywordIndex])) % this.N;
+        result += this.alphabet[n]; 
+      }
+      else
+      {
+        result += c;
+        continue;
+      }
+      keywordIndex++;
+      if(keywordIndex == key.length)
+      {
+        keywordIndex = 0;
+      }
+    }
+    return this.flag ? result : result.split('').reverse().join('');
   }
 }
 
