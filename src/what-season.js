24462040febba5b01f026d171d9694776114be42
27akim <criminal.ok1@gmail.com>
2021-03-11 15:53:46 +0300

    feat: add season

--- src/what-season.js
+++ src/what-season.js
@@ -1,26 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if(date === undefined)
-  {
-    return 'Unable to determine the time of year!';
+  if(!date) {
+    return "Unable to determine the time of year!";
   }
-  if(Object.prototype.toString.call(date) === '[object Date]')
-  {
-    throw new Error();
-  }
+
   const month = Date.prototype.getMonth.call(date);
+
   if (1 < month && month <= 4) {
     return "spring";
-  } 
- if (4 < month && month <= 7) {
+  } else if (4 < month && month <= 7) {
     return "summer";
-  }
-  if (7 < month && month <= 10) {
+  } else if (7 < month && month <= 10) {
     return "autumn";
-  }
-    else
-  {
-    return 'winter';
-  }
+  } else if (month === 11 ||
+    month === 0 ||
+    month === 1) {
+    return "winter";
+  }  
 };
